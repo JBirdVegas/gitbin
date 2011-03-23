d1e63922d2f1b4eddc7eb43c92c771720804f8d9
JBirdVegas <jbirdvegas@gmail.com>
2011-03-22 23:40:55 -0400

    initial distro setup script update

--- distrosetup.sh
+++ distrosetup.sh
@@ -1,2 +1,17 @@
+#su sudo first then execute script
+sudo ed -s /etc/rc.local <<< $'g/exit 0/d\nw'
 add-apt-repository "deb http://archive.canonical.com/ maverick partner"
-sudo apt-get install nautilus-gksu git curl build-essential zip
+sudo apt-get install nautilus-gksu curl build-essential zipexport fusedav fuseext2 fusefat fusesmb git-core git-doc git-gui
+cd ~/.ssh
+ssh-keygen -t rsa -C "jbirdvegas@gmail.com"
+mkdir -p ~/dev/gitbin
+mkdir ~/dev/build
+cd ~/dev/gitbin
+sudo usermod -a -G fuse $(id -u -n)
+git config --global user.name "JBirdVegas"
+git config --global user.email "jbirdvegas@gmail.com"
+sudo echo "PATH=$PATH:/home/jbird/bin:/home/jbird/dev/android-sdk-linux_x86/platform-tools:/home/jbird/Desktop/dev/scripts/build:/home/jbird/Desktop/dev/scripts/gitbin" >> /etc/rc.local
+sudo echo "exit 0" >> /etc/rc.local
+
+
+
