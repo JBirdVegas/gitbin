79dc72289e9c6844cdefc2d9325fc2ddd819b3dc
Jon Stanford <jbirdvegas@gmail.com>
2011-04-02 04:37:08 -0400

    trash removal + automount all partitions

--- automount.sh
+++ automount.sh
@@ -1,4 +1,6 @@
-sudo mkdir /mnt/LinuxMint
-sudo mount -t ext4 /dev/sda5 /mnt/LinuxMint
-sudo mkdir /mnt/Win7
+sudo mkdir /mnt/linuxmint
+sudo mount -t ext4 /dev/sda5 /mnt/linuxmint
+sudo mkdir /mnt/win7
 sudo mount /dev/sda3 /mnt/Win7
+sudo mkdir /mnt/xbuntu
+sudo mount -t ext4 /dev/sda7 /mnt/xbuntu
